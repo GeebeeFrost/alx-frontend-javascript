b0fac3196777dfeae277898843d01d4b40a9acee
GeebeeFrost <omgbolahan1@gmail.com>
2024-03-04 16:52:02 +0000

    Correct 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
-    return [task, task2];
+    var task;
+    var task2;
   }
 
   return [task, task2];
