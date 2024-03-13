ea2c7e191a33a7e85e8ae1e3bce8a77fe45e6784
GeebeeFrost <omgbolahan1@gmail.com>
2024-03-13 15:37:42 +0100

    Add TS interface for function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,15 @@ interface Teacher {
   location: string;
   [newProp: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
